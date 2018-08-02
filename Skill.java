32ff16239c73378efe557ad331271d8bb1bd9d60
alaskathecoder <34922645+alaskathecoder@users.noreply.github.com>
2018-08-02 16:32:09 -0400

    added comments for class structure

--- Skill.java
+++ Skill.java
@@ -1,4 +1,6 @@
 
 public interface Skill {
-
+	public static void main(String[] args) {
+		//string types int, agi, strength
+	}
 }
---
+++ Weapon.java
@@ -0,0 +1,8 @@
+
+public class Weapon {
+
+	public static void main(String[] args) {
+		//private short melee, long melee, short range, long range (pos 0, 1, 2, 3)
+	}
+
+}
